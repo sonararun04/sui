9b82972d577dad9a18ec791157767262a08c52b0
Ashok Menon <ashok@mystenlabs.com>
2023-04-05 17:54:09 +0300

    fixup: Also check that the child is found in its prev tx.

--- crates/sui-core/src/authority/authority_store.rs
+++ crates/sui-core/src/authority/authority_store.rs
@@ -1529,14 +1529,17 @@ impl ChildObjectResolver for AuthorityStore {
         let child_digest = child_object.digest();
         let prev_tx = child_object.previous_transaction;
         let effects = self.get_executed_effects(&prev_tx).unwrap().unwrap();
+        let mut found = false;
         for ((id, _, digest), owner) in effects.created().iter().chain(effects.mutated()) {
             if id == child {
+                found = true;
                 assert_eq!(digest, &child_digest, "Digest mismatch on {child} -> {parent}.\n\
                                                    Owner:    {owner:?}\n\
                                                    Expected: {child_digest}\n\
                                                    Found:    {digest}\n");
             }
         }
+        assert!(found, "Failed to find {child} -> {parent}");
 
         let parent = *parent;
         if child_object.owner != Owner::ObjectOwner(parent.into()) {
