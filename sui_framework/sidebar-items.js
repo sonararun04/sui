c8bcb7e40933799bafed9414f9895fcc98145182
amnn <amnn@users.noreply.github.com>
2023-04-08 15:21:17 +0000

    deploy: aa70f61cbec7ab676892b0ddd9f2d309ce6a3dfe

---
+++ sui_framework/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["DEFAULT_FRAMEWORK_PATH"],"fn":["build_move_package","legacy_test_cost"],"macro":["make_native"],"mod":["natives"],"struct":["BuiltInFramework","SystemPackage"]};
---
+++ sui_rpc_loadgen/load_test/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["get_tps"],"static":["CALLSITE","META"],"struct":["LoadTest","LoadTestConfig","WorkerThread"]};
